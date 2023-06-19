6294558c64ff105705f158f085f320a566740de5
sameerzf <sameer.zf025@gmail.com>
2023-06-19 16:01:51 +0500

    First day

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main(){
+runApp(MyApp());
+
+}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+  return MaterialApp(home: Center(child: Material(child: Container(child: Text("Welcome to MyAPP"),),)));
+  }
+}
