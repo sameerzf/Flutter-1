d87634a2a4108747cc4313b63b0fc2c0a478c970
sameerzf <sameer.zf025@gmail.com>
2023-07-03 12:27:59 +0500

    day 14 completed

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -42,11 +42,42 @@ class _HomePageState extends State<HomePage> {
       body: Padding(
         padding: const EdgeInsets.all(16.0),
         child: (CatalogModel.items != null && CatalogModel.items.isNotEmpty)
-            ? ListView.builder(
-                itemCount: CatalogModel.items.length,
+            ? // ListView.builder(
+            //     itemCount: CatalogModel.items.length,
+            //     itemBuilder: (context, index) {
+            //       return ItemWidget(item: CatalogModel.items[index]);
+            //     },
+            //   )
+            GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    mainAxisSpacing: 16,
+                    crossAxisSpacing: 16),
                 itemBuilder: (context, index) {
-                  return ItemWidget(item: CatalogModel.items[index]);
+                  final item = CatalogModel.items[index];
+                  return Card(
+                      clipBehavior: Clip.antiAlias,
+                      shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(10)),
+                      child: GridTile(
+                          child: Image.network(
+                            item.image,
+                          ),
+                          header: Container(
+                            child: Text(
+                              item.name,
+                              style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.bold),
+                            ),
+                            padding: EdgeInsets.all(12),
+                            decoration: const BoxDecoration(
+                              color: Colors.blueGrey,
+                            ),
+                          ),
+                          footer: Text(item.price.toString())));
                 },
+                itemCount: CatalogModel.items.length,
               )
             : Center(child: CircularProgressIndicator()),
       ),
